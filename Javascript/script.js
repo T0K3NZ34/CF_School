7ecdab3a2afd6a72753cdad9bda12553fe216402
Jorge Xavier De la Torre Landa <jorgex.delatorrelanda@gmail.com>
2025-02-24 11:35:17 +0100

    Bootstrap grid created and finished, starting with tables

---
+++ Javascript/script.js
@@ -0,0 +1,55 @@
+let age =  21;// number
+let name1 = "Jorge";
+let name2 = "De la Torre" //Strings
+let istudent = false; // this without "" is a boolean expression
+let pi = 3.14; //it still is a number
+const dog = "dog"; //const is ther to hardcode a value
+console.log(30 + age);
+
+//dog = "cat";//this is ot possible
+console.log(name1);
+// console.log(name1 + " " + name2);
+// console.log("hello i am"+" " + name1 +" "+ name2);
+console.log(`Hello i am ${name1} ${name2} and im a student = ${istudent}`)
+// this is better to use
+
+istudent= true;
+console.log(`Hello i am ${name1} ${name2} and im a student = ${istudent}`)
+
+//name caste
+
+
+//camel casee:thisIsAnExampleOfAnCamelCase
+
+//snake case: this_is_a_snake_case
+
+ let nr1 = 10;
+ let n2 = 20;
+//nr1 = nr1 +30
+// nr1+=30;
+// nr++; is going to oncrease the variable by 1
+//same happens for every other symbol / * - +
+ console.log(nr1);
+
+ console.log(nr1 + Number(n2));
+
+ // Number () method converts a string in to a number
+
+ console.log(typeof n20);
+ //this returns you the type of the variable
+ console.log(String(nr1));
+//  this converts a numer into a string
+
+let strigi ="hello World";
+console.log(strigi.length);
+console.log(strigi.charAt(0));
+console.log(strigi.toUpperCase());
+console.log(strigi.indexOf('h'));
+console.log(strigi. split("o"));
+let da = prompt(`hello kmafaka`);
+let ds = prompt (`enter second mumber`);
+console.log( da + ds);
+
+let by = Number(prompt(`what is your b year?`))
+
+console.log((2025 - by));
