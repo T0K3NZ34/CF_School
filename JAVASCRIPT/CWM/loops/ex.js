6e8d13a1c6335556941dc83faf42d68786e2fc3a
Jorge Xavier De la Torre Landa <jorgex.delatorrelanda@gmail.com>
2025-04-03 16:18:26 +0200

    learning

--- JAVASCRIPT/CWM/loops/ex.js
+++ JAVASCRIPT/CWM/loops/ex.js
@@ -72,3 +72,132 @@ function showNumbers(limit) {
   }
 }
 showNumbers(10);
+
+//////////
+//count the number of truthy values in a array
+//list of falsy values:
+//undefined
+//null
+// ''
+// false
+// 0
+// NaN
+
+// function countTruthy(array){
+//   let i = 0;
+//   let arrl = array.length;
+//   for (let i = 0; i < array.length; i++) {
+//     const detec = array[i];
+
+//     if (detec == 0 || detec == null || detec == false || detec == '' || detec == undefined || detec == NaN){
+//       arrl = arrl - 1;
+//     }
+//   }
+//   console.log(arrl);
+// }
+
+// countTruthy([NaN]);
+//NaN is not working here
+let array = [NaN];
+function countTruthy(array) {
+  let i = 0;
+  //for aof goes trough all the elements of the array
+  for (let value of array)
+    if (value)
+      // I dont need to type all of the fifferent falsy values
+      //if i dont specify the value of something the engine will try to interpret the, in this case the array as a boolean
+      i++;
+  return i;
+}
+console.log(countTruthy(array));
+
+///
+let obj = {
+  name: "jorge",
+  age: 22,
+  height: 180,
+  gf: "nina",
+};
+
+function showProperties(obj) {
+  for (const key in obj)
+    if (typeof obj[key] === "string") console.log(key, obj[key]);
+}
+//Typeof gives you the type of somethig
+//typeof gives you the type of something
+//typeof gives you the type of something
+showProperties(obj);
+
+/////
+// create a function of that returns all teh multiples of 3 and 5 from cero up to the given limit
+
+function sum(limit) {
+  let sum = 0;
+
+  for (let i = 0; i <= limit; i++) {
+    if (i % 3 === 0 || i % 5 === 0) sum += i;
+  }
+}
+console.log(sum(10));
+
+///
+
+const marks = [80, 60, 70];
+
+function calcGrade(marks) {
+  let letter = "";
+  let allgrade = marks.length;
+  let markSum = 0;
+
+  for (let i of marks) {
+    markSum += i;
+    i++;
+  }
+
+  let finalgrade = markSum / allgrade;
+
+  if (finalgrade >= 1 && finalgrade <= 59) {
+    letter = "F";
+  } else if (finalgrade >= 60 && finalgrade <= 69) {
+    letter = "D";
+  } else if (finalgrade >= 70 && finalgrade <= 79) {
+    letter = "C";
+  } else if (finalgrade >= 80 && finalgrade <= 89) {
+    letter = "B";
+  } else if (finalgrade >= 90 && finalgrade <= 100) {
+    letter = "A";
+  }
+  return letter;
+}
+console.log(calcGrade(marks));
+
+///
+// Creata a function that displays the ammount of rows given with the ammount of stars aw well
+
+function showStars(rows) {
+  for (let i = 1; i <= rows; i++) {
+    console.log("*".repeat(i));
+    //.repeat makes it possible to repeat the *, i dont need the \n because console log already does that for me
+  }
+}
+showStars(5);
+
+//
+//Create a function that takes a limit and show all the prime numbers until its limit
+
+// Prime number (whose factors are only 1 and itself) its anumber that can only be devided by itself and the number one
+//Composite number: is a number that can be devided by many numbers and have the reminder of 0
+
+function showPrime(limit) {
+  for (let number = 2; number <= limit; number++) {
+    let isprime = true;
+    for (let factor = 2; factor < number; factor++) {
+      if (number % factor === 0) {
+        isprime = false;
+        break;
+      }
+    }
+    if (isprime) console.log(number);
+    
+  }
+}
